dc169da62bdbf7c0c57b89d7814de3f96552936c
huangxiaohaohoward <huangxiaohao_fir@outlook.com>
2023-12-22 22:54:31 +0800

    ADD: class demo

--- lib/class_dart.dart
+++ lib/class_dart.dart
@@ -9,6 +9,30 @@ void base() {
   var p = Person("xiaoming", 18, height: 19.11);
   var p2 = Person.withNameAgeHeight("xiaoming", 18, 19.22);
   print(p);
+
+  var fp1 = FactoryPerson("h", "c");
+  var fp2 = FactoryPerson("h", "c");
+  print(identical(fp1, fp2));
+  var fp3 = FactoryPerson.withName("h");
+  var fp4 = FactoryPerson.withName("h");
+  print(identical(fp3, fp4));
+}
+
+class FactoryPerson {
+  String name;
+  String color;
+  static final Map<String, FactoryPerson> _nameCache = {};
+  static final Map<String, FactoryPerson> _colorCache = {};
+
+  FactoryPerson(this.name, this.color);
+
+  factory FactoryPerson.withName(String name) {
+    if (_nameCache.containsKey(name)) {
+      return _nameCache[name] ?? FactoryPerson("d", "d");
+    } else {
+      return FactoryPerson("d", "d");
+    }
+  }
 }
 
 class Person {
@@ -24,7 +48,8 @@ class Person {
   /*
   object 和 dynamic 的区别
   */
-  Person.withMap(Map<String, dynamic> map) {
+  Person.withMap(Map<String, dynamic> map,
+      {this.name = "d", this.age = 1, this.height = 1.1}) {
     print(map);
     this.age = map["name;"];
     this.name = map[" age;"];
